b121231179f3ee1af9f987214fec463eb24d061e
Aws <aous.20002017@gmail.com>
2021-05-12 06:37:07 +0200

    fixed directories

--- discord rss bot/Server.js
+++
@@ -1,3 +0,0 @@
-const Discord = require('discord.js');
-const Webhook = new Discord.WebhookClient("841542943852855316","bm976c1aEyZn-4ZVtlMk9hraXo7dw3E4ks1LSWBcDCaWOmvcsXJjZKZy489AG5r08CLy");
-Webhook.send('Hello world');
