9da64483b06ab93a94398f2e425fd995adc9a674
Aws <aous.20002017@gmail.com>
2021-05-12 11:39:18 +0200

    added env

--- Server.js
+++ Server.js
@@ -1,3 +1,21 @@
 const Discord = require('discord.js');
-const Webhook = new Discord.WebhookClient("841542943852855316","bm976c1aEyZn-4ZVtlMk9hraXo7dw3E4ks1LSWBcDCaWOmvcsXJjZKZy489AG5r08CLy");
+const config = require('dotenv').config()
+
+const Webhook = new Discord.WebhookClient(process.env.BOT_ID,process.env.BOT_TOKEN);
+
+let Parser = require('rss-parser');
+let parser = new Parser();
+
+(async () => {
+
+  let feed = await parser.parseURL('https://www.reddit.com/.rss');
+  console.log(feed.title);
+
+  feed.items.forEach(item => {
+    console.log(item.title + ':' + item.link)
+  });
+
+})();
+
+
 Webhook.send('Hello world');
