f326efd254d1112ad9f4931466c5debafffced26
Aws <aous.20002017@gmail.com>
2021-05-13 08:07:22 +0200

    added comparing new feed with old and updating old database

--- Server.js
+++ Server.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const config = require('dotenv').config()
-
+const fs = require('fs')
 const Webhook = new Discord.WebhookClient(process.env.BOT_ID,process.env.BOT_TOKEN);
 
 let Parser = require('rss-parser');
@@ -8,10 +8,32 @@ let parser = new Parser();
 var rsslist =  process.env.FESTIVALS.split(' ');
 
 (async () => {
+    let feed = [];
+    let feed2 = [];
+    
+    //getting last saved rss data from feeds.json
+    {
+    data = fs.readFileSync('Feeds.json', 'utf8') 
+    feed = JSON.parse(data.toString());
+    }
 
-    let i;let feed = [];
-    for (i = 0; i < rsslist.length; i++)   feed.push(await parser.parseURL(rsslist[i]));
-    for (i = 0; i < rsslist.length; i++)  feed[i].items.forEach(Item => {console.log(Item.title)});
-
+    //getting new feed and comparing with old data
+    let i;let news;
+    for (i = 0; i < rsslist.length; i++) {
+        feed2.push(await parser.parseURL(rsslist[i]));
+        feed[i].items.forEach(item => {
+            let j=0;
+            feed2[i].items.forEach(item2 => {
+                if (item.title == item2.title){j=1; break;}
+                if(j==0)news += item;
+            });
+        });
+    }
+    //update feeds.json
+    {
+    let StFeed; 
+    StFeed= JSON.stringify(feed2);
+    fs.writeFileSync('Feeds.json', StFeed);
+    }
 })();
 //Webhook.send('Hello world');
